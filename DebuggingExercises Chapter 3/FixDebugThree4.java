77d61c797cc043ea55e2d263cd487845d17ebd6f
John Brosius <brosius01@gmail.com>
2018-09-14 09:39:14 -0500

    Update FixDebugThree4.java

--- DebuggingExercises Chapter 3/FixDebugThree4.java
+++ DebuggingExercises Chapter 3/FixDebugThree4.java
@@ -9,7 +9,7 @@ public class FixDebugThree4
       System.out.println("My tuition:");
       tuitionBill(myCredits, rate);
       System.out.println("Your tuition:");
-      tuitionBill();
+      tuitionBill(yourCredits, myCredits);
    }
    public static void tuitionBill(int c double r)
    {
